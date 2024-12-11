09b8dfbc4f0b9ea55aa9f4a2749a283786a21583
IRjSI <rajusuthar0077@gmail.com>
2024-12-11 19:36:17 +0530

    Implement Input and Select components

--- MegaBlog/src/components/Header/Header.jsx
+++ MegaBlog/src/components/Header/Header.jsx
@@ -45,6 +45,22 @@ function Header() {
               <Logo />
             </Link>
           </div>
+          <ul className='flex ml-auto'>
+            {navItems.map((item) => (
+              item.active ?(
+                <li key={item.name}>
+                  <button onClick={() => navigate(item.slug)} className='inline-block px-6 py-2 duration-200 rounded-full'>
+                    {item.name}
+                  </button>
+                </li>
+              ) : null
+            ))}
+            {authStatus && (
+              <li>
+                <LogoutBtn />
+              </li>
+            )}
+          </ul>
         </nav>
       </Container>
     </header>
---
+++ MegaBlog/src/components/Input.jsx
@@ -0,0 +1,26 @@
+import React, {useId} from 'react'
+
+const input = React.forwardRef(function Input({
+    label,
+    type="text",
+    className='',
+    ...props
+},ref){
+    const id = useId();
+    return(
+        <div className='w-full'>
+            {label && <label
+            className='inline-block mb-1 pl-1'
+            htmlFor={id}>
+                {label}
+            </label>}
+            <input type={type}
+            className={`${className}`}
+            ref={ref}
+            />
+        </div>
+    )
+}
+)
+
+export default Input
