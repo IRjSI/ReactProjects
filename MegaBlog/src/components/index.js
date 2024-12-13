856a54b7fdeebd071510696ce411efb9b014a5f8
IRjSI <rajusuthar0077@gmail.com>
2024-12-13 13:13:22 +0530

    Provide routing

--- MegaBlog/src/components/index.js
+++ MegaBlog/src/components/index.js
@@ -2,12 +2,16 @@ import Header from './Header/Header'
 import Footer from './Footer/Footer'
 import Container from './Container/Container'
 import Logo from './Logo'
+import Input from './Input'
 import LogoutBtn from './Header/LogoutBtn' 
-import RTE from '../RTE'
+import RTE from '../components/RTE'
 import Signup from './Signup'
 import Login from './Login'
 import PostForm from './PostForm/PostForm'
 import Post from './PostCard'
+import AuthLayout from './AuthLayout'
+import Button from '../components/Button'
+import Select from './Select'
 
 export {
     Header,
@@ -19,5 +23,9 @@ export {
     Signup,
     Login,
     PostForm,
-    Post
+    Post,
+    AuthLayout,
+    Button,
+    Input,
+    Select
 }
